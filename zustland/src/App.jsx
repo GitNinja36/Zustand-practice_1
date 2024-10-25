52ea8e13b26287ba1a4e97e0d224d58fcbb26f8a
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2024-10-25 20:07:54 +0530

    learning more about zustand

--- zustland/src/App.jsx
+++ zustland/src/App.jsx
@@ -1,14 +1,18 @@
-import { create } from 'zustand'
 import './App.css'
+import { create } from 'zustand'
+import {devtools, persist} from 'zustand/middleware'
 
 const useStore = create((set) => ({
   num : 0,
-  increaseNumber : ()=>set((state)=>({num : state.num + 1})),
-  decreaseNumber : () =>set((state)=>({num: state.num -1}))
+  increaseNumber : () => set((state)=>({num : state.num + 1})),
+  decreaseNumber : () => set((state)=>({num: state.num -1})),
+  increaseBy : (n) => set((state)=>({num : state.num + n})),
+  decreaseBy : (n) => set((state)=>({num : state.num - n})),
+  reset : () => set({num : 0})
 }))
 
 function App() {
-  const {num, increaseNumber, decreaseNumber} = useStore();
+  const {num, increaseNumber, decreaseNumber, increaseBy, decreaseBy, reset} = useStore();
   return (
     <>
     <div className='container'>
@@ -20,6 +24,9 @@ function App() {
       <div>
         <button onClick={increaseNumber}>increase</button>
         <button onClick={decreaseNumber}>decrease</button>
+        <button onClick={() => increaseBy(10)}>increase by 10</button>
+        <button onClick={() => decreaseBy(10)}>decrease by 10</button>
+        <button onClick={reset}>reset</button>
       </div>
     </div>
     </>
